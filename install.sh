c1a1b05455f882e28d142c5f3addfb4f3c25d2d3
a359566926 <1210968738@qq.com>
2017-07-30 22:51:20 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# install
+#sudo apt-get update && sudo apt-get install -y python python-pip
+#pip install shadowsocks
+
+# config
+cd ~
+config_file='shadowsocks/config.json'
+config_file_path=`dirname $config_file`
+mkdir -p $config_file_path && echo \
+'{\n'                                   \
+'    "server":"0.0.0.0",\n'             \
+'    "server_port":6666,\n'             \
+'    "local_address":"127.0.0.1",\n'    \
+'    "local_port":1080,\n'              \
+'    "password":"lzmhaha",\n'           \
+'    "timeout":300,\n'                  \
+'    "method":"aes-256-cfb",\n'         \
+'    "fast_open":false\n'               \
+'}\n'                                   \
+ > $config_file
+
+# start(https://stackoverflow.com/questions/12996397/command-not-found-when-using-sudo)
+echo 'sudo -E env "PATH=$PATH" ssserver -c ~/'$config_file' -d start' > $config_file_path/start.sh
